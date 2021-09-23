9ffc19c350ccef789d4927b97b680f2ced094e2e
John Wernecke <john@Johns-MacBook-Air.local>
2021-09-23 15:42:09 -0500

    style fixes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -183,21 +183,21 @@
 			<div class="flex flex-col sm:flex-row mt-2 mb-10 sm:mb-32">
 				<div>
 					<p class="uppercase font-semibold text-[15px] tracking-wider mt-5 mb-4">Who We Are</p>
-					<p class="sm:max-w-[660px] text-[14px] sm:text-[20px] font-light leading-8 sm:leading-9">
+					<p class="sm:max-w-[660px] text-[14px] sm:text-[20px] leading-8 sm:leading-9">
 						Our culture is all about autonomy, creativity, and collaboration. Respect is guaranteed,
 						taking initiative is highly encouraged, and challenging the status quo is expected.
 						Above all else, we work hard and try to not take ourselves too seriously along the way.
 					</p>
 					<p class="uppercase font-semibold text-[15px] tracking-wider mt-14 mb-4">Where We Work</p>
-					<p class="sm:max-w-[660px] text-[14px] sm:text-[20px] font-light leading-8 sm:leading-9">
+					<p class="sm:max-w-[660px] text-[14px] sm:text-[20px] leading-8 sm:leading-9">
 						Our headquarters are based in Chicago. We currently have a flexible work from home
 						policy that includes a mix of in-office and fully remote employees.
 					</p>
 					<p class="uppercase font-semibold text-[15px] tracking-wider mt-14 mb-4">What We Offer</p>
 
 					<ul
-						class="sm:max-w-[660px]text-[14px] sm:text-[20px] list-disc list-inside font-light
-						leading-7 sm:leading-9">
+						class="sm:max-w-[660px] text-[14px] sm:text-[20px] list-disc list-inside leading-7
+						sm:leading-9">
 						<li class="pt-2">Flexible PTO policy with a required minimum</li>
 						<li class="pt-2">Health & dental insurance coverage</li>
 						<li class="pt-2">Competitive parental leave policy</li>
